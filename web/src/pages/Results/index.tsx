50826044360f49afa310abcbde54dc4073b7e9c0
gustavodaltoe <gustavodaltoe@hotmail.com>
2020-09-16 22:46:10 -0300

    Ajustes de layout em Results

--- web/src/pages/Results/index.tsx
+++ web/src/pages/Results/index.tsx
@@ -7,68 +7,52 @@ import './styles.scss';
 function Results() {
   return (
     <Base>
-      <fieldset>
-        <legend>Resultados</legend>
-        <br />
-        <br />
-
+      <div id="results">
+        <h1>Resultados</h1>
         <div className="profile-results">
           <div>
             <strong>Taxa metábólica basal:</strong>
             <span>1791 kcal</span>
           </div>
-          <br />
 
           <div>
             <strong>Índice massa corporal:</strong>
             <span>29.41</span>
           </div>
-          <br />
 
           <div>
             <strong>Requisitos de água:</strong>
             <span>2750 ml</span>
           </div>
-          <br />
 
           <div>
             <strong>Requisitos calóricos diários:</strong>
             <span>1970 kcal</span>
           </div>
-          <br />
         </div>
-        <br />
-        <br />
 
-        <div className="side-by-side">
-          <div id="font-red">
+        <div className="macros">
+          <div className="font-red">
             <span>Carboidratos</span>
-
             <strong>172 g</strong>
-
             <span>688 kcal</span>
           </div>
-          <div id="font-blue">
+          <div className="font-blue">
             <span>Proteína</span>
-
             <strong>172 g</strong>
-
             <span>688 kcal</span>
           </div>
-          <div id="font-yellow">
+          <div className="font-yellow">
             <span>Gordura</span>
-
             <strong>66 g</strong>
-
             <span>594 kcal</span>
           </div>
         </div>
-        <br />
 
         <button type="button" className="btn-green">
           Continuar
         </button>
-      </fieldset>
+      </div>
     </Base>
   );
 }
