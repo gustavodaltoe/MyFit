25e387e351250f8b5c06aacf252d7204677c6c32
gustavodaltoe <gustavodaltoe@hotmail.com>
2020-11-06 20:28:35 -0300

    conexao com o banco de dados

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-import { FoodModule } from './food/food.module';
 
 @Module({
-  imports: [UserModule, FoodModule],
+  imports: [TypeOrmModule.forRoot(), UserModule],
 })
 export class AppModule {}
