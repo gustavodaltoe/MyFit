85039c0cf2712e078c66b44279f879e35795917e
gustavodaltoe <gustavodaltoe@hotmail.com>
2020-11-06 21:01:24 -0300

    food module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { FoodModule } from './food/food.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule],
+  imports: [TypeOrmModule.forRoot(), UserModule, FoodModule],
 })
 export class AppModule {}
