11da96d87d7d7822b0268aceca03df67e6df4b58
gustavodaltoe <gustavodaltoe@hotmail.com>
2020-11-06 21:18:06 -0300

    global reflectors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,10 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   await app.listen(3333);
 }
 bootstrap();
