20050a139f3fdde7b5b8cf99ca46bf5dabf6f920
gustavodaltoe <gustavodaltoe@hotmail.com>
2020-11-06 21:20:52 -0300

    add global pipes e class validator

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,17 @@
-import { ClassSerializerInterceptor } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { json } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.use(
+    json({
+      limit: '50mb',
+    }),
+  );
   await app.listen(3333);
 }
 bootstrap();
