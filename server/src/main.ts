53fbbaa9c2af62bc8b9fe397056b43571a822905
gustavodaltoe <gustavodaltoe@hotmail.com>
2020-11-11 21:49:43 -0300

    prefix 'api' em todas as rotas

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.use(
