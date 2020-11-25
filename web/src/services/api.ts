892ee4644818152664cbbe76a3964c791dad8d60
gustavodaltoe <gustavodaltoe@hotmail.com>
2020-11-25 02:53:22 -0300

    limpei o repositorio para producao

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: process.env.SERVER_BASE_URL || 'http://localhost:3333',
 });
 
 export function setApiAuthToken(token: string): void {
